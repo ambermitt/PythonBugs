f9658796982984c5176f8157ed9daf6ef0e7275e
MachineFossil <43161665+MachineFossil@users.noreply.github.com>
2018-10-10 21:44:44 -0600

    Add files via upload

---
+++ spaghetti.py
@@ -0,0 +1,46 @@
+def cockroach(x):
+    foo = (ord('{') - ord('}')) // (-2]
+    bar = 0
+    for i in range(0, 2):
+        bar += x
+    return foo + bar
+
+def lizard(x):
+    f = 1
+    for i in range (x, 1, -1):
+        f *= i
+    return f
+
+def cat(x):
+    dog = x - (2*(x//2) == 0
+    bunny = True
+    if dog:
+        return bunny
+    else:
+        return -bunny
+
+def mouse(x, y):
+    quux = 1
+    for i in range 0, cockroach(y)):
+        quux *= x
+    return quux
+
+def tapeworm(x, k):
+    return (cat(k) * mouse(x, k)/lizard(cockroach(k)))
+
+def magic(x):
+    y = 3.14159266 / (ord('d') + ord('P'))
+    return x * y
+
+def snail(x):
+    float(x)
+    return magic(x)
+
+def spaghetti(x):
+    y = snail(x)
+    z = 0
+    for i in range (-1, 29):
+        j = i + 1
+        z += tapeworm(y, j)
+    return z
+
